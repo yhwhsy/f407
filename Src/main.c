906cc30c26ded0fec291d11e4cd3702ce6e16fea
yhwhsy <1924655993@qq.com>
2026-03-16 17:58:08 +0800

    openi2c

--- Src/main.c
+++ Src/main.c
@@ -21,6 +21,7 @@
 #include "adc.h"
 #include "dcmi.h"
 #include "dma.h"
+#include "i2c.h"
 #include "spi.h"
 #include "usart.h"
 #include "gpio.h"
@@ -124,6 +125,7 @@ int main(void)
   MX_SPI1_Init();
   MX_USART3_UART_Init();
   MX_ADC1_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
   /* 初始化TFT */
   ST7789_Init(&hspi1);
